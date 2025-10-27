2a548a0e78e1696a90d540ea13376b744aaac10c
Zlata Stefanović <zlatastefanovic@Zlatas-MacBook-Air-2.local>
2025-10-27 13:43:37 +0800

    update files to the newest version

--- MessageBoardClient.py
+++ MessageBoardClient.py
@@ -1,66 +1,76 @@
 import socket
 import time
 
-SERVER_ADDRESS = '127.0.0.1'
-SERVER_PORT = 16111
+#SERVER_ADDRESS = '127.0.0.1'
+#SERVER_PORT = 16111
 BUFFER_SIZE = 4096
 SENDING_COOLDOWN = 0.3
 
 def send_message(sock, message):
-    sock.sendall(bytes(message + "\n", encoding='utf-8'))
     time.sleep(SENDING_COOLDOWN)
+    sock.sendall(bytes(message , encoding='utf-8'))
 
 def receive_message(sock):
     data = sock.recv(BUFFER_SIZE)
-    return data.decode('utf-8').strip()
+    return str(data, encoding='utf-8')
+
+def deletefunc(sock):
+    while True:
+        message_id = input("client: ")
+        send_message(sock, message_id)
+        if message_id.strip() == "#":
+            break
+    response = receive_message(sock)
+    print("server:", response)
+
+def getfunc(sock):
+    while True:
+        response = receive_message(sock)
+        if response == '#':
+            break
+        elif response == "":
+            break
+        print("server:", response)
+    print("server:", response)
+
+def quitfunc(sock):
+    response = receive_message(sock)
+    print("server:", response)
+
+def postfunc(sock):
+    while True:
+        line = input("client: ")
+        send_message(sock, line)
+        if line.strip() == "#": #end of the message
+            break
+    response = receive_message(sock)
+    print("server:", response)
+
+def diffcommand(sock):
+    response = receive_message(sock)
+    print("server:", response)
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_sock:
-        client_sock.connect((SERVER_ADDRESS, SERVER_PORT))
+    SERVER_ADDRESS = input() #get the server IP
+    SERVER_PORT = int(input()) #get the port number
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         
+        sock.connect((SERVER_ADDRESS, SERVER_PORT))
         while True:
-            flag = False # Used in the edge case with "POST" function
             command = input("client: ").strip().upper() #getting the command
-            send_message(client_sock, command)
+            send_message(sock, command)
             
             if command == 'POST':
-                while True:
-                    line = input("client: ")
-                    send_message(client_sock, line)
-                    if line.strip() == "#": #end of the message
-                        flag = True #edge case
-                        break
-                response = receive_message(client_sock)
-                print("server:", response)
-            
+                postfunc(sock)
             elif command == 'GET':
-                while True:
-                    response = receive_message(client_sock)
-                    if response == '#':
-                        break
-                    elif response == "":
-                        break
-                    print("server:", response)
-                print("server: #")
-
-
-            
+                getfunc(sock)
             elif command == 'DELETE':
-                while True:
-                    message_id = input("client: ")
-                    send_message(client_sock, message_id)
-                    if message_id.strip() == "#":
-                        break
-                response = receive_message(client_sock)
-                print("server:", response)
-            
+                deletefunc(sock)
             elif command == 'QUIT':
-                response = receive_message(client_sock)
-                print("server:", response)
+                quitfunc(sock)
                 break
-
-            elif not flag: #ensures that it is not end of the 'POST' function
-                print("server: ERROR - Command not understood")
+            else:
+                diffcommand(sock)
 
 if __name__ == '__main__':
     main()
